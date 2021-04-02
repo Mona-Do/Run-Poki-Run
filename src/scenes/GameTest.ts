551cbb8a1cdab439e9529cac267bcb93b6dfefb7
Mona Dong <mdong4@id.iit.edu>
2021-04-03 01:03:44 +0800

    made the platforms move instead of the player

--- src/scenes/GameTest.ts
+++ src/scenes/GameTest.ts
@@ -1,36 +1,28 @@
 import Phaser from 'phaser';
-
 import createPokiAnims from '../anims/poki.js';
 
 export default class GameTest extends Phaser.Scene {
+  background: Phaser.GameObjects.TileSprite;
   color1: Phaser.Display.Color;
   color2: Phaser.Display.Color;
-  w: number;
-  h: number;
-  scrollX: number;
-  rightEdge: number;
+  player: any;
   platformGroup: Phaser.GameObjects.Group;
-  platformPosition: number;
-  timer: number;
-  timeText: Phaser.GameObjects.Text;
-  spike1: any;
-  platformPool: any;
+  platformPool: Phaser.GameObjects.Group;
   nextPlatformDistance: number;
-  spawnRange: number;
+  cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+  spawnRange: any;
+  plarformSizeRange: number[];
+  platformSizeRange: number[];
 
   constructor() {
     super('gametest');
   }
 
-  player;
-  platform;
-  cursors;
-  background;
-  hexColor;
-
   preload() {}
 
   create() {
+    // this.game = new Phaser.Game(config);
+
     //background
     //make the layer-meme repeat
     let width = this.scale.width;
@@ -39,62 +31,35 @@ export default class GameTest extends Phaser.Scene {
       .tileSprite(40, 0, width, height, 'layer-meme')
       .setOrigin(0, 0)
       .setScrollFactor(0, 0);
+    // this.background.setVelocityX(-350);
 
     //set the background color
     this.color1 = new Phaser.Display.Color(105, 59, 76);
     this.color2 = new Phaser.Display.Color(105, 70, 0);
 
-    //need to add an object pool
-    this.platform = this.physics.add.staticGroup();
-    this.platform.create(0, 400, 'platform');
-    // this.platform.create(400, 400, 'platform');
-    // this.platform.create(900, 400, 'platform');
-    // this.platform.create(1300, 400, 'platform');
-    // this.platform.create(1800, 400, 'platform');
-    // this.platform.create(2400, 400, 'platform');
-
-    //set the spike behind
-    this.spike1 = this.add.image(0, 0, 'spike-behind').setOrigin(0, 0);
-    this.spike1.setScrollFactor(0, 0);
-
     //player
     this.player = this.physics.add.sprite(150, 300, 'poki');
-    // this.player.body.setGravityY(0);
-    this.player.body.setVelocityX(250);
-    this.physics.add.collider(this.player, this.platform);
+    this.player.body.setGravityY(900);
     createPokiAnims(this.anims);
     this.player.anims.play('run');
 
-    //set camera
-    this.cameras.main.startFollow(this.player);
-    this.cameras.main.setBounds(0, 0, Number.MAX_SAFE_INTEGER, 500);
-    this.w = this.cameras.main.width;
-    this.h = this.cameras.main.height;
 
-    //platform group
-    // group with all active platforms.
-    //this.platform.body.setGravityY(0);
+    // create group
     this.platformGroup = this.add.group({
-      // once a platform is removed, it's added to the pool
-      defaultKey: this.platform
-      // removeCallback: function (platform) {
-      //   platform.scene.platformPool.add(platform);
-      // },
-    });
-    this.platformGroup = this.physics.add.staticGroup();
-
-    // pool
-    this.platformPool = this.add.group({
-      // once a platform is removed from the pool, it's added to the active platforms group
       removeCallback: function (platform) {
-        this.platformGroup.add(platform);
+        platform.scene.platformPool.add(platform);
       },
     });
 
-    // adding a platform to the game, the arguments are platform width and x position
-    this.addPlatform(this.scale.width, this.scale.width / 2);
+    // create pool
+    this.platformPool = this.add.group({
+      removeCallback: function (platform) {
+        platform.scene.platformGroup.add(platform);
+      },
+    });
 
-    // setting collisions between the player and the platform group
+    // add platform
+    this.addPlatform(width, width/2);
     this.physics.add.collider(this.player, this.platformGroup);
   }
 
@@ -110,43 +75,51 @@ export default class GameTest extends Phaser.Scene {
     } else {
       platform = this.physics.add.sprite(
         posX,
-        this.scale.height * 0.8,
+        400,
         'platform'
       );
       platform.setImmovable(true);
-      platform.setVelocityX(-100);
+      platform.setVelocityX(-350);
       this.platformGroup.add(platform);
     }
     platform.displayWidth = platformWidth;
+    this.spawnRange = [110, 300];
     this.nextPlatformDistance = Phaser.Math.Between(
-      (this.spawnRange = 100),
-      (this.spawnRange = 350)
-    );
-
-    //set the timer
-    //make it stick on the top right
-    this.timeText = this.add.text(900, 20, 'Time Survived:');
-    this.timeText.setScrollFactor(0, 0);
+        this.spawnRange[0],
+        this.spawnRange[1]
+      );
   }
 
-  update() {
-    //make the background scroll
-    this.background.setTilePosition(this.cameras.main.scrollX);
 
-    //make the background color change
-    let hexColor = Phaser.Display.Color.Interpolate.ColorWithColor(
-      this.color1,
-      this.color2,
-      -this.h * 2,
-      this.player.x
-    );
-    this.cameras.main.setBackgroundColor(hexColor);
+  update() {
+
+    this.player.x = 200;
+
+    // recycling platforms
+    let minDistance = 1142;
+    this.platformGroup.getChildren().forEach(function (platform) {
+      let platformDistance = 1142 - platform.x - platform.displayWidth / 2;
+      minDistance = Math.min(minDistance, platformDistance);
+      if (platform.x < -platform.displayWidth / 2) {
+        this.platformGroup.killAndHide(platform);
+        this.platformGroup.remove(platform);
+      }
+    }, this);
+
+    // adding new platforms
+    if (minDistance > this.nextPlatformDistance) {
+      this.platformSizeRange = [100, 250];
+      var nextPlatformWidth = Phaser.Math.Between(
+        this.platformSizeRange[0],
+        this.platformSizeRange[1]
+      );
+      this.addPlatform(nextPlatformWidth, 1142 + nextPlatformWidth / 2);
+    }
 
     //poki move
     this.cursors = this.input.keyboard.createCursorKeys();
-
     if (this.cursors.space?.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-200);
+      this.player.setVelocityY(-400);
       //this.player.anims.play('jump', true);
     }
 
@@ -154,9 +127,5 @@ export default class GameTest extends Phaser.Scene {
       //this.player.anims.play('die', true);
       this.scene.start('gameover');
     }
-
-    //timer
-    this.timer = this.time.now * 0.001;
-    this.timeText.setText('Time Survived: ' + Math.round(this.timer));
   }
 }
